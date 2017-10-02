109a896534d70b3e5c8f2274c4d4a8f0e78c4f60
lbiro <biro89laszlo@gmail.com>
2017-10-02 17:27:08 +0200

    booklib.c: created a new file. It contains the implementation of the struct vector library program described in Programozzunk C nyelven 'exl. mark' Section 3.7.5.

---
+++ booklib.c
@@ -0,0 +1,43 @@
+// This program contains an implementation of the example given in
+// "Programozzunk C nyelven!" Section 3.7.5.
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#define N 100
+
+struct book {
+	char * title;
+	char * author;
+	int date;
+	double price;
+};
+
+void fillbooklib(struct book *booklib);
+void searchbooklib(struct book *resultlib, struct book *booklib);
+
+int main(void)
+{
+
+	int k;
+	int num_match;
+	struct book *booklib[N];
+	struct book *resultlib[N];
+	for (k = 0; k < N; k++) {
+		booklib[k] = malloc(sizeof(struct book));
+		resultlib[k] = malloc(sizeof(struct book));
+		if (0 == booklib[k] || 0 == resultlib[k]) {
+			printf("Error! Not enough memory!\n");
+			exit(-1);
+		}
+	}
+	// Part 2: Filling booklib
+	
+	// Part 3: Searching in booklib, filling resultlib
+	for (k = 0; k < N; k++) {
+		free(booklib[k]);
+		free(resultlib[k]);
+	}
+	return 0;
+}
