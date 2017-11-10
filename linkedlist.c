7c20d58f87b8d856a14237d3a26755346e798169
lbiro <biro89laszlo@gmail.com>
2017-11-10 17:05:17 +0100

    Write list printing and pushing functions
    linkedlist.c: I copied the pushing and printing functions from the
    homepage from which I learn programming.

--- linkedlist.c
+++ linkedlist.c
@@ -6,9 +6,33 @@ struct node {
         struct node * next;
 };
 
+void print_list(struct node * head)
+{
+        struct node * current = head;
+        while (current != NULL) {
+                printf("%d\n", current->val);
+                current = current->next;
+        }
+        return;
+}
+
+void push(struct node * head, int val)
+{
+        struct node * current = head;
+        while(current->next != NULL) {
+                current = current->next;
+        }
+        current->next = malloc(sizeof(struct node));
+        current->next->val = val;
+        current->next->next = NULL;
+        return;
+}
+//The push function comes now
+
 int main(void)
 { 
         struct node * head = NULL;
+        head = malloc(sizeof(struct node));
         if (head == NULL) {
                 return 1;
         }
